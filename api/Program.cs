dfd371badc9f231e8e0331d4f1898c164e256d11
Przemek <psoszynski@gmail.com>
2026-07-17 13:25:50 +0200

    feat: enable Application Insights telemetry in Azure Functions backend

--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Worker;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
     .Build();
 
 host.Run();
